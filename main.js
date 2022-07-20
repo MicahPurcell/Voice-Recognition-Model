a52f6f6d584537715c131370904cac8dce31b8d1
MicahPurcell <93412875+MicahPurcell@users.noreply.github.com>
2022-07-19 21:04:13 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+x = 0;
+y = 0;
+draw_circle= ''
+draw_rectangle= ''
+
+var SpeechRecognition = window.webkitSpeechRecognition
+var recognition = new SpeechRecognition
+
+function start(){
+
+    document.getElementById("status").innerHTML='System is listening, please say circle or rectangle.'
+    recognition.start()
+}
+
+recognition.onresult = function(event){
+    console.log(event);
+
+    
+    var content = event.results[0][0].trascript
+
+    if (content=="Circle.") {
+        x = Math.floor(Math.random() * 900)
+        y = Math.floor(Math.random() * 600)
+        draw_circle= "set"
+        
+        
+    }
+
+    if (content=="Rectangle.") {
+        x = Math.floor(Math.random() * 900)
+        y = Math.floor(Math.random() * 600)
+        draw_rectangle= "set"
+    }
+
+
+}
+
+function draw(){
+
+    R= Math.floor(Math.random() * 255)
+    G= Math.floor(Math.random() * 255)
+    B= Math.floor(Math.random() * 255)
+    fill(R,G,B)
+    stroke(R,G,B)
+    if (draw_circle=='set') {
+        radius=Math.floor(Math.random() * 100)
+        circle(x,y,radius)
+        document.getElementById('status').innerHTML="Circle has been drawn, click draw if you wan to draw another shape."
+    }
+
+    if (draw_rectangle=='set') {
+        rect(x,y,50,70)
+        document.getElementById('status').innerHTML="Rectangle has been drawn, click draw if you wan to draw another shape."
+
+    }
+}
+
+function setup(){
+    canvas = createCanvas(900,600)
+}
